b7ef85e5cc1dddae0828dca0218dac321335562e
shraddha <shraddhashetty10@gmail.com>
2021-08-14 16:46:46 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void iosApps()
+	{
+	System.out.println("iosApps");
+	}
+
+
+}
